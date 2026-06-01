c3aba14b18c98f71fc2d5acc24fc23e6af077fb1
shobhasinghps-cloud <shobhasingh.ps@gmail.com>
2026-06-01 06:10:30 +0530

    Update BuildConfig.kt

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,6 +1,6 @@
 object BuildConfig {
     const val MINECRAFT_VERSION: String = "1.21.1"
-    const val FABRIC_LOADER_VERSION: String = "0.18.4"
+    const val FABRIC_LOADER_VERSION: String = "0.16.9"
     const val NEOFORGE_VERSION: String = "26.1.0.1-beta"
     const val FABRIC_API_VERSION: String = "0.144.3+26.1"
     const val UKULIB_VERSION: String = "2.0.0+26.1"
